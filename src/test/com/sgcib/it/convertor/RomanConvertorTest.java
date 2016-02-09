ca3dc6bb3a8d0abf2fbe3458e2e24b57f16bba7e
helmi gouider <gouider.helmi@gmail.com>
2016-02-09 10:16:14 +0100

    apply changes to test method names

--- src/test/com/sgcib/it/convertor/RomanConvertorTest.java
+++ src/test/com/sgcib/it/convertor/RomanConvertorTest.java
@@ -4,7 +4,7 @@ import org.junit.*;
 public class RomanConvertorTest {
 
 	@Test
-	public void testRomanConvertorCase1(){
+	public void testConvertNumberToRomanWithHundreds(){
 		System.out.println("test convertor with value 89, result should be : LXXXIX" );
 		String result = RomanConvertor.convertNumberToRoman(89);
 		System.out.println("the result is " + result);
@@ -12,11 +12,16 @@ public class RomanConvertorTest {
 	}
 	
 	@Test
-	public void testRomanConvertorCase2(){
+	public void testConvertNumberToRomanWithThousands(){
 		System.out.println("test convertor with value 1245, result should be : MCCXLV" );
 		String result = RomanConvertor.convertNumberToRoman(1245);
 		System.out.println("the result is " + result);
 		Assert.assertEquals("MCCXLV", result);
+	}
+	
+	@Test(expected=NumberFormatException.class)
+	public void testConvertNumberToRomanWithNegativeValue() {
+		System.out.println(RomanConvertor.convertNumberToRoman(-99));
 	}
 	
 }
